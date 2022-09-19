0b325de1c429afc3bdba79673e6952c2ee8667b9
silentmantra <silentmantra.ru@gmail.com>
2022-09-19 17:18:37 +0200

    add support to select doctors for futher editing

---
+++ src/components/CheckboxSelect.vue
@@ -0,0 +1,29 @@
+<script setup>
+
+import { useModel } from '@/common/useModel';
+import { computed, reactive } from 'vue';
+import Checkbox from './Checkbox.vue';
+
+const props = defineProps('modelValue items'.words);
+const value = useModel();
+
+const items = reactive([]);
+for (const id in props.items) {
+    items.push(reactive({
+        id,
+        title: props.items[id],
+        selected: computed({
+            get: () => value.value === id,
+            set: val => {
+                value.value = val ? id : null
+            }
+        })
+    }));
+}
+
+
+</script>
+
+<template>
+    <Checkbox v-for="item in items" v-model="item.selected" :key="item.id"><span v-html="item.title"></span></Checkbox>
+</template>
