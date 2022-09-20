66f331e914dfb5709bdb47091b361f0bb47ec33f
silentmantra <silentmantra.ru@gmail.com>
2022-09-20 10:18:17 +0200

    add resize trigger to DynamicTeleport

--- src/components/DynamicTeleport.vue
+++ src/components/DynamicTeleport.vue
@@ -1,6 +1,6 @@
 <script setup>
 
-import { ref, onMounted } from 'vue';
+import { ref, onMounted, onUpdated } from 'vue';
 import { propsToRefs } from '@/common/utils';
 
 defineProps('to disabled'.words);
@@ -10,6 +10,16 @@ const { to, disabled } = propsToRefs();
 const target = ref();
 
 onMounted(() => target.value = document.querySelector(to.value));
+
+let resized = false;
+
+onUpdated(()=> {
+    if (resized) {
+        return;
+    }
+    window.dispatchEvent(new Event('resize'));
+    resized = true;
+});
 
 </script>
 
